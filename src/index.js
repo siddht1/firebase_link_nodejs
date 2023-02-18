794e2118c739d8dbdc493241a84d65fd238aea2b
siddht1 <rt1yrru+github@gmail.com>
2023-02-18 20:58:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,15 +16,15 @@ const serviceAccount={
   "auth_uri":  process.env.FIREBASE_AUTH_URI,
   "token_uri": process.env.FIREBASE_TOKEN_URI,
   "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
-  "databaseURL":  process.env.FIREBASE_DATABASE_URL,
-  "authDomain": process.env.FIREBASE_AUTH_DOMAIN
+  "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
+//   "databaseURL":  process.env.FIREBASE_DATABASE_URL,
+//   "authDomain": process.env.FIREBASE_AUTH_DOMAIN
   
 };
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-//   databaseURL:  process.env.FIREBASE_DATABASE_URL,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL:  process.env.FIREBASE_DATABASE_URL,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN
 });
 
 // // Firebase database reference
