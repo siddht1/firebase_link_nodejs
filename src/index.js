3badae785891ba286be5b17aec0c49de0bf212ee
siddht1 <rt1yrru+github@gmail.com>
2023-02-18 20:55:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -28,17 +28,17 @@ admin.initializeApp({
 });
 
 // // Firebase database reference
-// const db = admin.database();
-// const userRef = db.ref("users");
+const db = admin.database();
+const userRef = db.ref("users");
 
-// Add a new user
-// const addUser = (obj, res) => {
-//   const oneUser = userRef.child(obj.roll);
-//   oneUser
-//     .set(obj)
-//     .then(() => res.status(200).json({ msg: "User created successfully" }))
-//     .catch((err) => res.status(300).json({ msg: "Something went wrong", error: err }));
-// };
+//Add a new user
+const addUser = (obj, res) => {
+  const oneUser = userRef.child(obj.roll);
+  oneUser
+    .set(obj)
+    .then(() => res.status(200).json({ msg: "User created successfully" }))
+    .catch((err) => res.status(300).json({ msg: "Something went wrong", error: err }));
+};
 
 // // Add a demo user
 // const demoUser = (obj, res) => {
