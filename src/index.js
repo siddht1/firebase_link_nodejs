507bf2c22bbfe269afaa998dac504128a7b778b9
siddht1 <rt1yrru+github@gmail.com>
2023-02-23 12:39:56 +0530

    Update index.js
    set maximum data limit to 50 MB for testing purpose,will reduce it to 1MB

--- src/index.js
+++ src/index.js
@@ -8,8 +8,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 //cors enabled
 const cors = require('cors');
-// Middleware to parse JSON data
-app.use(express.json());
+// Middleware to parse JSON data and extend data limit to 50 MB
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+// Set the body-parser middleware with the limit property set to 50 MB
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
 // Enable CORS for all routes
 app.use(cors());
 // Firebase config
